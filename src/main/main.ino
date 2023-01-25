9501e08645227b84663ba02fa1306509e8198e8f
Martin Oganisyan <martin@oganisyan.de>
2023-01-26 00:00:33 +0100

    Add HardwareReset to command interpreter. - > Allows firmware upload only with TX and RX wiring, without DTR wiring.

--- src/main/main.ino
+++ src/main/main.ino
@@ -2,6 +2,7 @@
 #include <Wire.h>
 #include "Tone.h"
 #include "Reader.h"
+#include <avr/wdt.h>
 
 #include "MS5611.h"
 
@@ -10,28 +11,40 @@
 #define BEEP_PIN            A2 // PC2
 //#define RX_PIN            10 // PB2 
 //#define TX_PIN            9  // PB1
+#define RESET_PIN           2
 
 
-float calibrate(float freq, float downMin, float downMax, float upMin, float upMax ) 
+float calibrate(float freq, float downMin, float downMax, float upMin, float upMax )
 {
   freq -= constrain(freq, downMin, upMin);
   return  constrain(freq, downMax, upMax);
 }
 
 
-float toneFreq, toneFreqLowpass, pressure, lowpassFast, lowpassSlow;
+float   toneFreq, toneFreqLowpass, pressure, lowpassFast, lowpassSlow;
 int16_t ddsAcc;
 
 
 
-MS5611   ms5611(Wire);
-Tone myTone;
+MS5611 ms5611(Wire);
+Tone   myTone;
 Reader reader(Serial);
 
-
+void hardwareReset(uint32_t milis) {
+  Serial.end();
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(RESET_PIN, OUTPUT);
+  digitalWrite(0,LOW);
+  digitalWrite(1,LOW);
+  delay(milis);
+  digitalWrite(RESET_PIN,LOW);
+}
 
 void setup() {
+  digitalWrite(RESET_PIN,HIGH);
   Serial.begin(57600);
+  Serial.print("Start");
   ms5611.begin();
   
   while(!ms5611.isReady()) {
@@ -47,15 +60,20 @@ void setup() {
 
 }
 
-
 void loop() {  // run over and over
-
   reader.loop();
   if(reader.hasLine()) {
     String cmd = reader.getLine();
-    if(cmd.startsWith("$BVL ") && cmd.indexOf('*') > 0 ) {
-      int value = cmd.substring(5, cmd.indexOf('*')).toInt();
+    if(cmd.startsWith("$BVL") && cmd.indexOf('*') > 0 ) {
+      int startIndex = 4;
+      while(!isgraph(cmd[startIndex])) startIndex++;
+      int value = cmd.substring(startIndex, cmd.indexOf('*')).toInt();
       myTone.setVolume(value);
+    } else if(cmd.startsWith("$RST") && cmd.indexOf('*') > 0 ) {
+      int startIndex = 4;
+      while(!isgraph(cmd[startIndex])) startIndex++;
+      int value = cmd.substring(startIndex, cmd.indexOf('*')).toInt();
+      hardwareReset(value*1000);
     }
   }
   
